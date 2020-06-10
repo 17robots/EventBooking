d97b17f381e31fa5146397ca98022a81e28033eb
17robots <mdray@ameritech.net>
2020-06-10 14:17:59 -0400

    Added API Hit for Auth

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200)
+    }
+    next()
+})
+
 app.use(isAuth)
 
 app.use('/graphql', graphQLHttp({
@@ -20,7 +30,8 @@ app.use('/graphql', graphQLHttp({
 }))
 
 mongoose.connect(`mongodb+srv://${process.env.user}:${process.env.password}@projects-wtsk3.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`).then(() => {
-    app.listen(3000)  
-}).catch(err => {
+    app.listen(8000)  
+})
+.catch(err => {
     console.log(err)
 })
